7af5987acbc0ecacdb7ce45bd46dbb024e331179
franciscosucre <franciscojsucreg@gmail.com>
2017-02-12 14:54:42 -0400

    (FJSG) Arreglo en la logica del ejercicio

--- Problema 7/simulacion.py
+++ Problema 7/simulacion.py
@@ -20,6 +20,15 @@ def iniciar_simulacion(maximo_dias, maximo_punto_reorden):
         envio_en_camino = False
 		# Iniciamos la simulacion para un nuevo punto de reorden
         for dias_transcurridos in range(maximo_dias):
+        	# Si habia un envio en camino pero ya llego, recibimos el envio
+            if dias_para_proximo_envio == 0 and envio_en_camino:
+            	inventario_actual += Q
+                costo_acumulado += COSTO_POR_PEDIDO
+                envio_en_camino = False
+            # Si hay un envio en camino pero no ha llegado, reducimos el tiempo
+            elif dias_para_proximo_envio > 0 and envio_en_camino:
+            	dias_para_proximo_envio -= 1
+            # 
             demanda_del_dia = random_demand()
             inventario_actual -= demanda_del_dia
             # Manejo del costo de la escasez
@@ -29,18 +38,11 @@ def iniciar_simulacion(maximo_dias, maximo_punto_reorden):
             elif inventario_actual > 0:
                 costo_acumulado += inventario_actual * COSTO_POR_MANTENER
 			# Si estamos por debajo del punto de reorden, hacemos un pedido
-            if inventario_actual <= punto_reorden:
+            if inventario_actual <= punto_reorden and not envio_en_camino:
             	dias_para_proximo_envio = random_delivery_time()
-            	envio_en_camino = False
-            # Si habia un envio en camino pero ya llego, recibimos el envio
-            if dias_para_proximo_envio == 0 and envio_en_camino:
-            	inventario_actual += Q
-                costo_acumulado += COSTO_POR_PEDIDO
-                envio_en_camino = True
-            # Si hay un envio en camino pero no ha llegado, reducimos el tiempo
-            elif dias_para_proximo_envio > 0 and envio_en_camino:
-            	dias_para_proximo_envio -= 1
-
+            	envio_en_camino = True
+            
+		# Si el punto de reorden en cuestion es el primero, no lo comparo
         if punto_reorden == 0:
             costo_minimo = costo_acumulado
             reorden_optimo = punto_reorden
@@ -49,4 +51,4 @@ def iniciar_simulacion(maximo_dias, maximo_punto_reorden):
             reorden_optimo = punto_reorden
 
     print "La punto de reorden optimo es %d unidades con costo $%d " % (reorden_optimo, costo_minimo)
-    return costo_acumulado
+    return [reorden_optimo,costo_minimo]
